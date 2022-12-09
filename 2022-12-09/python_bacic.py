c40371fe0b4d6eb66c5d300519895d1ee72f3843
viswajithpm <018vpm@gmail.com>
2022-12-09 18:08:08 +0530

    pythonbasics

---
+++ 2022-12-09/python_bacic.py
@@ -0,0 +1,95 @@
+# string="This is a string"
+# print(string)
+# print(string[1])
+# print(string[-1])
+# print(string[:])
+# print(string[0:12])
+# print(string[0:12:2])
+# print(string[::-1])
+# str1="apple"
+# str2="red"
+# print(str1,"is",str2)
+# print(str1+" is "+str2)
+# print("%s is %s"%(str1,str2))
+# print("{} is {}".format(str1,str2))
+# print(type(string))
+
+# list1=[10,9,8,7,6,5,4,3,2,1,0,None]
+# list2=list1.copy()
+# list1.append("a")
+# list1.insert(0,"b")
+# list1.extend(list2)
+# list1.remove(3)
+# list1.pop(2)
+# list1.clear()
+# print(list1.count(1))
+# print(list1.index(2))
+# list1.sort()
+# list1.reverse()
+# print(list1[1::2])
+
+# lst=[i for i in range(10)]
+# print(list1)
+
+# tuple1=(1,2,3,4,2,2,2,2)
+# print(tuple1[1])
+# print(tuple1.count(2))
+# print(tuple1.index(4))
+
+# dictionary={"1":"apple","2":"orange","3":"kiwi"}
+# print(dictionary)
+# print(dictionary['3'])
+# print(dictionary.get('1'))
+# print(dictionary.values())
+# print(dictionary.keys())
+# print(dictionary.items())
+# print(dictionary.popitem())
+# print(dictionary.pop("2"))
+# dictionary.clear()
+# dictionary.update({"3":"mango"})
+# print(dictionary)
+
+# set1={1,2,3,4,5,}
+# set2={1,2,4,9,10}
+# set1.add("h")
+# set1.update("kiwi")
+# set1.update([9,8,7,])
+# set3=set1.union(set2)
+# set3=set2.difference(set1)
+# print(set3)
+# non=None
+# print(non)
+
+# for i in range(10):
+# 	if i==8:
+# 		continue
+# 	print(i)
+# else:
+# 	print("Finished")
+# i=10
+# while(i):
+# 	print(i)
+# 	i=i-1
+
+# def add(x,y):
+# 	return x+y
+# print(add(1,3))
+
+# def number():
+# 	print("hello")
+# 	number()
+# number()
+# def num(x,y):
+	# print(x+y)
+# num(y=9,x=10)
+
+# class Animal:
+# 	name="cat"
+# obj=Animal()
+# print(obj.name)
+
+# class Mobile:
+# 	def calling(self):
+# 		print("calling")
+# mob1=Mobile()
+# mob1.calling()
