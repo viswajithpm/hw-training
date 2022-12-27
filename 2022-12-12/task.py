9f0ee2c33917b1aadaba361a77203117561b5dee
viswajithpm <018vpm@gmail.com>
2022-12-27 16:53:18 +0530

    task.py updated

--- 2022-12-12/task.py
+++ 2022-12-12/task.py
@@ -6,16 +6,14 @@ class Employee:
 	def __init__(self):
 		self.log={}
 		self.task_list=[]
-		self.nme=""
-	def login(self):
-		eid=input("Enter employee id:")
-		name=input("Enter employee name:")
-		self.nme=name
+		self.name=""
+	def login(self, emp_id, emp_name):
+		self.name=emp_name
 		login_time=datetime.datetime.now().isoformat().replace("T"," ")[0:16]
 		print("Login successful")
 		self.log.update({
-			'id':eid,
-			'name':name,
+			'id':emp_id,
+			'name':emp_name,
 			'login':login_time
 			})	
 	def logout(self):
@@ -24,13 +22,11 @@ class Employee:
 		self.log.update({'logout':logout_time})
 		dt=date.today()
 		dict_str=json.dumps(self.log,indent=1)
-		filename=f'{dt}_{self.nme}.json'
+		filename=f'{dt}_{self.name}.json'
 		file=open(filename,"w")
 		file.write(dict_str)
-	def addTask(self):
+	def addTask(self, title, description):
 		self.task_dic={}
-		title=input("Enter the Task Title: ")
-		description=input("Enter the description: ")
 		start_time=datetime.datetime.now().isoformat().replace("T"," ")[0:16]
 		e=int(input("Task added\n Press 1 when finished."))
 		self.task_dic.update({
@@ -39,22 +35,24 @@ class Employee:
 			'start_time':start_time
 			})
 		if e==1:
-			obj_eid.endTask()
-	def endTask(self):
+			obj_eid.endTask("flag")
+	def endTask(self, flag):
 		end_time=datetime.datetime.now().isoformat().replace("T"," ")[0:16]
-		flag=input("Wheater the task was successful ?(Enter True or False)")
-		self.task_dic.update({'end_time':end_time,'successful':flag})
+		self.task_dic.update({
+			'end_time':end_time,
+			'successful':flag
+			})
 		self.task_list.append((self.task_dic))
 		self.log.update({'task':self.task_list})
 obj_eid=Employee()
-obj_eid.login()
+obj_eid.login("100","Athul")
 print("=====================")
 while True:
 	print("Enter a option:")
 	choice=int(input("1.AddTask\n2.Logout\n"))
 	if choice==1:
-		obj_eid.addTask()
-	elif ch==2:
+		obj_eid.addTask("title","description")
+	elif choice==2:
 		obj_eid.logout()
 		break
 	else:
