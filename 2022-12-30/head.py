fa312f004e5df113c75ae858c2dee2732c333c96
viswajithpm <018vpm@gmail.com>
2022-12-30 15:24:31 +0530

    logging module added

--- 2022-12-30/head.py
+++ 2022-12-30/head.py
@@ -31,3 +31,15 @@
 # print(unparse_url)
 # request_url = urllib.request.urlopen('https://www.geeksforgeeks.org/')
 # print(request_url.read())
+
+import logging
+
+logging.basicConfig(filename="newfile.log",format='%(asctime)s %(message)s',filemode='w')
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+logger.debug("Harmless debug message")
+logger.info("Just a information")
+logger.warning("It's a warning")
+logger.error("Error occured")
+logger.critical("Internet is down")
+
