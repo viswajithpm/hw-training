4f19da5467cd8d398486366eb88681e5e7af9534
viswajithpm <018vpm@gmail.com>
2023-01-02 18:01:39 +0530

    updated with optimized code

--- 2022-12-07/mytheresa.py
+++ 2022-12-07/mytheresa.py
@@ -3,26 +3,34 @@ from parsel import Selector
 import json
 
 class Mytheresa:
+
     def __init__(self):
         self.url = 'https://www.mytheresa.com/int_en/men/shoes.html'
-        self.headers={'user-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'}
+        self.headers={
+        'user-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+        }
+
     def parse(self):
-        list1=[]
-        result = requests.get(self.url,headers=self.headers)
-        if result:
-            text=result.text
-            selector=Selector(text=text)
+        list1 = []
+        result = requests.get(self.url, headers=self.headers)
+
+        if result.status_code == 200:
+            text = result.text
+            selector = Selector(text=text)
+
             amount = selector.xpath('//div[contains(@class,"product-shop")]')
             product = selector.xpath("//div[@class='main']")
             price = amount.xpath('//div[contains(@class,"price-info pa1-rmm-price")]')
-            image = selector.css('img.gallery-image').xpath('@src').get()
-            image_url = "https:%s"%image
+            image = selector.xpath('//img[contains(@class,"gallery-image")]/@src').get()
+            image_url = f"https:{image}"
             sizes = []
-            size_details=selector.xpath("//div[@class='product-shop'] //ul[@class='sizes'] //span/text()").getall()
+
+            size_details=selector.xpath('//ul[contains(@class,"sizes")]//span/text()').getall()
             for size in size_details:
                 size = size.replace("/", " ").strip(" ").split("  ")
                 for i in size:
                     sizes.append(i)
+
             images = selector.css('img.lazyload').xpath('@data-src').getall()
             other_images = []
             for image in images:
@@ -54,20 +62,21 @@ class Mytheresa:
                 "other images":other_images,
                 }
         list1.append(dictionary)
-        dis_string=json.dumps(list1,indent=1)
-        f=open("mytheresa.json","w")
+        dis_string=json.dumps(list1, indent=1)
+        f=open("mytheresa.json", "w")
         f.write(dis_string)
+
     def parse_link(self):
-        result = requests.get(self.url,headers=self.headers)
+        result = requests.get(self.url, headers=self.headers)
         if result.status_code == 200:
-            text=result.text
-            selector1=Selector(text=text)
+            text = result.text
+            selector1 = Selector(text=text)
             for link in selector1.xpath('//a[contains(@class,"product-image")]/@href'):
                 parse(link.get(),self.headers)
+
             next_page=selector1.xpath('//li[contains(@class,"next")]/a/@href').get()
             if next_page is not None:
-                parse_link(next_page,self.headers)
-        else:
-            print("Error\n",result)
+                parse_link(next_page, self.headers)
+
 mytheresa = Mytheresa()
 mytheresa.parse_link()
