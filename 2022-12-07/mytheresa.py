be0fd9d1f304661cc263d30c6686a9344c033dbf
viswajithpm <018vpm@gmail.com>
2023-01-03 18:33:12 +0530

    code updated and optimized

--- 2022-12-07/mytheresa.py
+++ 2022-12-07/mytheresa.py
@@ -20,89 +20,70 @@ class Mytheresa:
     def parse(self, url):
         list1 = []
         result = requests.get(url=url, headers=self.headers)
-
-        if result.status_code == 200:
-            text = result.text
-            selector = Selector(text=text)
+        if result.status_code != 200:
+            pass
+        else:
+            selector = Selector(text=result.text)
 
             amount = selector.xpath('//div[contains(@class,"product-shop")]')
             product = selector.xpath("//div[@class='main']")
             price = selector.xpath('//div[contains(@class,"price-info pa1-rmm-price")]')
 
-            image = selector.xpath('//img[contains(@class,"gallery-image")]/@src').get()
+            image = selector.xpath('//img[contains(@class,"gallery-image")]/@src').extract_first()
             image_url = f"https:{image}"
 
             sizes = []
-            size_details = selector.xpath('//ul[contains(@class,"sizes")]//span/text()').getall()
+            size_details = selector.xpath('//ul[contains(@class,"sizes")]//span/text()').extract()
             for size in size_details:
                 size = size.split('/')
                 for sze in size:
                     if sze.strip():
                         sizes.append(sze.strip())
 
-            images = selector.xpath('//img[contains(@class,"gallery-image")]/@data-src').getall()
+            images = selector.xpath('//img[contains(@class,"gallery-image")]/@data-src').extract()
             other_images = []
             for image in images:
                 imageurl = f"https:{image}"
                 other_images.append(imageurl)
 
-            description = (str(product.xpath('//p[contains(@class,"product-description")]/text()').get())) + ' '.join(product.xpath("//li[@class='pa1-rmm']/text()").getall())
+            description = (str(product.xpath('//p[contains(@class,"product-description")]/text()').extract_first())) + ' '.join(product.xpath("//li[@class='pa1-rmm']/text()").extract())
+            listing_price = price.xpath('/div[contains(@class,"price-box")]/span/span/text()').extract_first()
 
-            if price.xpath('/div[contains(@class,"price-box")]/span/span/text()').get() is not None:
+            if listing_price is None:
+                listing_price = price.xpath('//div/p[contains(@class,"old-price")]/span/text()').extract_first()
 
-                dictionary = {
+            dictionary = {
 
-                "breadcrumbs":selector.xpath('//div[contains(@class,"breadcrumbs")]//span/text()').getall(),
-                "image url":image_url,
-                "brand":selector.xpath('//div[contains(@class,"product-designer")]/span/a/text()').get(),
-                "product name":selector.xpath('//div[contains(@class,"product-name")]/span/text()').get(),
-                "product id": amount.xpath('//span[contains(@class,"h1")]/text()').get().replace('item no.\xa0',''),
-                "price":price.xpath('/div[contains(@class,"price-box")]/span/span/text()').get(),
-                "sizes":sizes,
-                "description":description,
-                "other images":other_images,
+            "breadcrumbs":selector.xpath('//div[contains(@class,"breadcrumbs")]//span/text()').extract(),
+            "image url":image_url,
+            "brand":selector.xpath('//div[contains(@class,"product-designer")]/span/a/text()').extract_first(),
+            "product name":selector.xpath('//div[contains(@class,"product-name")]/span/text()').extract_first(),
+            "product id": amount.xpath('//span[contains(@class,"h1")]/text()').extract_first().replace('item no.\xa0',''),
+            "price":listing_price,
+            "special_price":price.xpath('//div/p[contains(@class,"special-price")]/span/text()').extract_first(),
+            "discount":price.xpath('//span[contains(@class,"price-reduction-notice")]/text()').extract_first().replace("off","").strip(),
+            "sizes":sizes,
+            "description":description,
+            "other images":other_images,
 
-                }
-            else:
-
-                dictionary = {
-
-                "breadcrumbs":selector.xpath('//div[contains(@class,"breadcrumbs")]//span/text()').getall(),
-                "image url":image_url,
-                "brand":selector.xpath('//div[contains(@class,"product-designer")]/span/a/text()').get(),
-                "product name":selector.xpath('//div[contains(@class,"product-name")]/span/text()').get(),
-                "product id": amount.xpath('//span[contains(@class,"h1")]/text()').get().replace('item no.\xa0',''),
-                "price":price.xpath('//div/p[contains(@class,"old-price")]/span/text()').get(),
-                "special_price":price.xpath('//div/p[contains(@class,"special-price")]/span/text()').get(),
-                "discount":price.xpath('//span[contains(@class,"price-reduction-notice")]/text()').get().replace("off","").strip(),
-                "sizes":sizes,
-                "description":description,
-                "other images":other_images,
-
-                }
+            }
         with open('mytheresa2.csv','a') as csvfile:
-
             writer = csv.DictWriter(csvfile, fieldnames=self.fields)
             writer.writerow(dictionary)
-            # writer.writeheader()
-
-        # list1.append(dictionary)
-        # dis_string=json.dumps(list1, indent=1)
-        # f=open("mytheresa.json", "a")
-        # f.write(dis_string)
 
     def parse_link(self, url):
 
         result = requests.get(url=url, headers=self.headers)
-        if result.status_code == 200:
-            text = result.text
-            selector1 = Selector(text=text)
-            for link in selector1.xpath('//a[contains(@class,"product-image")]/@href'):
-                mytheresa.parse(link.get())
+        if result.status_code != 200:
+            pass
+        else:
+            selector1 = Selector(text=result.text)
+            for link in selector1.xpath('//a[contains(@class,"product-image")]/@href').extract():
+                mytheresa.parse(link)
 
-            next_page=selector1.xpath('//li[contains(@class,"next")]/a/@href').get()
-            if next_page is not None:
-                mytheresa.parse_link(next_page)
+        next_page=selector1.xpath('//li[contains(@class,"next")]/a/@href').extract_first()
+        if next_page is not None:
+            mytheresa.parse_link(next_page)
 
 url = 'https://www.mytheresa.com/int_en/men/shoes.html'
 mytheresa = Mytheresa()
--- 2023-01-03/training.py
+++ 2023-01-03/training.py
@@ -1,34 +1,9 @@
-import csv
-import os.path
-
-file_exists = os.path.isfile('name.csv')
-
-d = {
-	'first':'Baked',
-	'last':'Beans',
-	}
-with open('name.csv','w')as cf:
-	fields = ['first','last','middle']
-	writer = csv.DictWriter(cf,fieldnames=fields)
-	for i in range(5):
-		if file_exists:
-			writer.writerow(d)
-		else:
-			writer.writeheader()
-			writer.writerow(d)
-
-
-
-# import os.path
-
-
-# file_exists = os.path.isfile(filename)
-
-# with open (filename, 'a') as csvfile:
-#     headers = ['TimeStamp', 'light', 'Proximity']
-#     writer = csv.DictWriter(csvfile, delimiter=',', lineterminator='\n',fieldnames=headers)
-
-#     if not file_exists:
-#         writer.writeheader()  # file doesn't exist yet, write a header
-
-#     writer.writerow({'TimeStamp': dic['ts'], 'light': dic['light'], 'Proximity': dic['prox']})
+def one(i):
+	lst = [100,200,300,400,20,500,700,00]
+	if i != 200:
+		raise Exception("Error")
+	else:
+		print(i+1)
+		for i in lst:
+			one(i)
+one(200)
